4d66df81669f4608e0a5cfdd1209ed1dbc6485b4
divyanshi-2612 <161453596+divyanshi-2612@users.noreply.github.com>
2025-06-17 19:40:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Anagram of String/anagram-of-string.java
@@ -0,0 +1,23 @@
+/*Complete the function below*/
+class GfG {
+    public int remAnagrams(String s1, String s2) {
+        // add code here.
+        int f1[] = new int[26];
+        int f2[] = new int[26];
+        
+        for (char ch:s1.toCharArray())
+        {
+            f1[ch-'a']++;
+        }
+        for (char ch:s2.toCharArray())
+        {
+            f2[ch-'a']++;
+        }
+        int count=0;
+        for(int i=0; i<26; i++)
+        {
+            count+=Math.abs(f1[i] - f2[i]);
+        }
+        return  count;
+    }
+}
